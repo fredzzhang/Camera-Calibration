d3dbf5a6c6d5372e42fcbba3571deda9db479f7d
FredericZhang <dzreal520@gmail.com>
2018-05-15 19:06:25 +1000

    compute normalization matrix for a cluster of 3d points

---
+++ getNormMat3d.m
@@ -0,0 +1,31 @@
+function Nmatrix = getNormMat3d(x)
+% Function: Compute the normalization matrix of dd points in 
+%           homogeneous coordinate
+% Normalization criteria:
+%   1. Move the centroidto the origin
+%   2. Average distance of  points to the centroid is sqrt(2) 
+% 
+% Usage:
+% 
+%       Nmatrix = getNormMat(x)
+%   where:
+%       Nmatrix - the normalization matrix
+%       x - input data, dim: 4xN
+% 
+% Institute: Australian National University
+% Author: Zhen Zhang
+% Last modified: 13 May 2018
+
+% Get the centroid
+centroid = mean(x, 2);
+% Compute the distance to the centroid
+dist = sqrt(sum((x - repmat(centroid, 1, size(x, 2))) .^ 2, 1));
+% Get the mean distance
+mean_dist = mean(dist);
+% Craft normalization matrix
+Nmatrix = [sqrt(2) / mean_dist, 0, 0, -sqrt(2) / mean_dist * centroid(1); ...
+           0, sqrt(2) / mean_dist, 0, -sqrt(2) / mean_dist * centroid(2); ...
+           0, 0, sqrt(2) / mean_dist, -sqrt(2) / mean_dist * centroid(3); ...
+           0, 0, 0, 1];
+
+end
